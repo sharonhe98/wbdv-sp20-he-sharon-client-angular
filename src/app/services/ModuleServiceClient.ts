625cbd1f0caf16f6812ef6c270e69f297591a696
unknown <he.sha@husky.neu.edu>
2020-03-27 15:02:08 -0400

    [FIX] Missing end brace

--- src/app/services/ModuleServiceClient.ts
+++ src/app/services/ModuleServiceClient.ts
@@ -4,3 +4,4 @@ import {Injectable} from '@angular/core';
 export class CourseServiceClient {
   findModulesForCourseId = (courseId) => fetch(`http://wbdv-generic-server.herokuapp.com/api/001694524/courses/${courseId}/modules`)
     .then(response => response.json())
+}
