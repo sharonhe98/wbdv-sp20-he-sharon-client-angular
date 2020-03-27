325e716423ed0f77eb61a6a5db53950537de264a
unknown <he.sha@husky.neu.edu>
2020-03-27 14:01:57 -0400

    [ADD] Add app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import {CourseListComponent} from './course-list/course-list.component';
+import {CourseViewerComponent} from './course-viewer/course-viewer.component';
+import {NgModule} from '@angular/core';
+
+const routes: Routes = [
+  {path: '', component: CourseListComponent},
+  {path: 'course/:cid', component: CourseViewerComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,7 @@ import { CourseListComponent } from './course-list/course-list.component';
 import {FormsModule} from '@angular/forms';
 import {CourseServiceClient} from './services/course.service.client';
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
-import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-
-const routes: Routes = [
-  {path: '/', component: CourseListComponent},
-  {path: 'course/:cid', component: CourseViewerComponent},
-];
-
 
 @NgModule({
   declarations: [
